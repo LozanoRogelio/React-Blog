d45749475c9e6e840b5efbf46d2a18aef6c9a7d3
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-09-09 16:44:35 -0500

    Created components for navbar and home page, learned to import into app js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
 
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {50} times</p>
+        <Home></Home>
+        
 
-        <p>{10}</p>
-        <p></p>
       </div>
     </div>
   );
