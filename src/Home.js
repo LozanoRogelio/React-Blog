53fbf2f26c76c4899ba3bead0c98503de6c4793c
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-09-14 11:59:39 -0500

    Used useState to set a loading message while data was being fetchd. Also uset timeout to simulate real server loading time

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,26 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState (null);
+    const [isPending, setIsPending] = useState(true);
 
   
 
     useEffect(() => {
+    setTimeout(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
             return res.json();
         })
         .then(data => {
             setBlogs(data);
-        })
-    }, [])
+            setIsPending(false)
+        });
+    }, 5000);
+    }, []);
     
     return (  
         <div className="home">
+            {isPending && <div>Loading...</div>}
            { blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         
 
