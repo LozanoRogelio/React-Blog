0170ad983ace35457aac349881bbb5f1372f24dc
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-09-16 12:19:42 -0500

    Able to submit events with ahandle submit funciton and using onSubmit on my form

--- src/Create.js
+++ src/Create.js
@@ -5,12 +5,18 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+
+        console.log(blog)
+    }
 
 
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title:</label>
                 <input 
                 type="text" 
@@ -31,9 +37,7 @@ const Create = () => {
                     <option value="yoshi">yoshi</option>
                 </select>
                 <button>Add Blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+                
 
             </form>
         </div>
